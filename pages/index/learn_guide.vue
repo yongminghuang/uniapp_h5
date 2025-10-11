8c859cd839fe5666ac40bdcbf3e48d9394b1f6d3
hym <uffanv@gmail.com>
2025-10-11 09:35:42 +0800

    备份代码

--- pages/index/learn_guide.vue
+++ pages/index/learn_guide.vue
@@ -1,13 +1,5 @@
 <template>
-	<view :class=" isShowBottom ? `container`:`container2`">
-		<!-- 顶部导航 -->
-		<!-- 	<view class="header">
-			<view class="back" @click="goBack">
-				<text class="iconfont">&#xe600;</text>
-			</view>
-			<text class="title">驾考学习指南</text>
-		</view> -->
-
+	<view :class="isShowBottom ? 'container' : 'container2'">
 		<!-- 欢迎语 -->
 		<view class="welcome">
 			<image class="welcome_bg" src="/static/guide/top_bg.png"></image>
@@ -18,58 +10,54 @@
 		<view class="quick-learning">
 			<swiper class="swiper_wrap" :current="current" indicator-dots="true" autoplay="true">
 				<swiper-item v-for="(item, index) in swiperList" :key="index">
-
 					<image class="swiper-item-image" :src="item.image" mode="scaleToFill" />
-
 				</swiper-item>
 			</swiper>
-
 		</view>
-		<!-- 模拟成绩展示 -->
 
+		<!-- 双行跑马灯 -->
 		<view class="double-marquee-container">
 			<!-- 第一行：恭喜消息 -->
 			<view class="marquee-row">
-				<view class="marquee-content" ref="row1">
+				<view class="marquee-content" ref="marqueeRow1">
 					<view class="marquee-item" v-for="(msg, index) in list1" :key="index">
 						<image class="icon" src="/static/guide/ic_broadcast.png"></image>
 						<span class="msg-text-color-1">恭喜</span>
 						<span class="msg-text-color-2">「{{ msg.userName }}」</span>
 						<span class="msg-text-color-1">模拟考试 </span>
-						<span class="msg-text-color-2">{{msg.score }}分</span>
+						<span class="msg-text-color-2">{{ msg.score }}分</span>
 					</view>
-					<!-- 复制一份，实现无缝 -->
+					<!-- 复制一份 -->
 					<view class="marquee-item" v-for="(msg, index) in list1" :key="index + list1.length">
+						<image class="icon" src="/static/guide/ic_broadcast.png"></image>
 						<span class="msg-text-color-1">恭喜</span>
 						<span class="msg-text-color-2">「{{ msg.userName }}」</span>
 						<span class="msg-text-color-1">模拟考试 </span>
-						<span class="msg-text-color-2">{{msg.score }}分</span>
+						<span class="msg-text-color-2">{{ msg.score }}分</span>
 					</view>
 				</view>
 			</view>
 
 			<!-- 第二行：系统通知 -->
 			<view class="marquee-row">
-				<view class="marquee-content" ref="row2">
+				<view class="marquee-content" ref="marqueeRow2">
 					<view class="marquee-item" v-for="(msg, index) in list2" :key="index">
 						<image class="icon" src="/static/guide/ic_broadcast.png"></image>
 						<span class="msg-text-color-1">恭喜</span>
 						<span class="msg-text-color-2">「{{ msg.userName }}」</span>
 						<span class="msg-text-color-1">模拟考试 </span>
-						<span class="msg-text-color-2">{{msg.score }}分</span>
+						<span class="msg-text-color-2">{{ msg.score }}分</span>
 					</view>
-					<!-- 复制一份 -->
 					<view class="marquee-item" v-for="(msg, index) in list2" :key="index + list2.length">
 						<image class="icon" src="/static/guide/ic_broadcast.png"></image>
 						<span class="msg-text-color-1">恭喜</span>
 						<span class="msg-text-color-2">「{{ msg.userName }}」</span>
 						<span class="msg-text-color-1">模拟考试 </span>
-						<span class="msg-text-color-2">{{msg.score }}分</span>
+						<span class="msg-text-color-2">{{ msg.score }}分</span>
 					</view>
 				</view>
 			</view>
 		</view>
-
 
 		<!-- 视频播放器 -->
 		<view class="video-container">
@@ -78,21 +66,14 @@
 
 		<!-- 功能卡片 -->
 		<view class="card-list">
-			<!-- 技巧练题 -->
 			<view class="card-item" @click="toSkillPage()">
 				<image class="card-item-image" src="/static/guide/bg_skill.png" mode="widthFix"></image>
-
 			</view>
-
-			<!-- 直播讲题 -->
 			<view class="card-item" @click="toLivePage()">
 				<image class="card-item-image" src="/static/guide/bg_live.png" mode="widthFix"></image>
 			</view>
-
-			<!-- 模拟真实考场 -->
 			<view class="card-item" @click="toMkExamPage()">
 				<image class="card-item-image" src="/static/guide/bg_exam.png" mode="widthFix"></image>
-
 			</view>
 		</view>
 
@@ -112,7 +93,9 @@
 		data() {
 			return {
 				isShowBottom: false,
-				marqueeSpeedSet: false, // 新增标志位
+				marqueeAnimId1: null,
+				marqueeAnimId2: null,
+				marqueeSpeed: 2, // 控制滚动速度（像素/帧）
 				swiperList: [{
 						image: '/static/guide/swiper_km1.png'
 					},
@@ -120,8 +103,7 @@
 						image: '/static/guide/swiper_km4.png'
 					}
 				],
-				current: 0, // 当前显示的轮播图索引
-				// 第一行：恭喜类消息
+				current: 0,
 				list1: [{
 						"userName": "朝霞云彩",
 						"score": 99
@@ -135,7 +117,6 @@
 						"score": 96
 					}
 				],
-				// 第二行：通知类消息
 				list2: [{
 						"userName": "朝霞云彩",
 						"score": 99
@@ -149,7 +130,7 @@
 						"score": 96
 					},
 					{
-						"userName": "凉然、鎖心碎",
+						"userName": "凉然、鎖心碎",
 						"score": 96
 					},
 					{
@@ -159,154 +140,137 @@
 					{
 						"userName": "永恒",
 						"score": 95
-					},
+					}
 				]
 			};
 		},
 		mounted() {
-			// this.$nextTick(() => {
-			// 	if (!this.marqueeSpeedSet) {
-			// 		this.adjustMarqueeSpeed('row1', this.list1);
-			// 		this.adjustMarqueeSpeed('row2', this.list2);
-			// 		this.marqueeSpeedSet = true;
-			// 	}
-			// });
-		},
-		created() {
-			let that = this;
-			this.isShowBottom = option.showBottomOpenVip == "1"
-			// window.showBottomOpenVip = (str) => {
-			// 	alert("str:" + str)
-			// 	console.log("iOS 主动推送的字符串：", str);
-			// 	that.isShowBottom = str == "1"
-
-			// };
+			this.$nextTick(() => {
+				// 延迟确保 DOM 渲染完成
+				setTimeout(() => {
+					this.startMarquee('marqueeRow1', this.list1);
+					this.startMarquee('marqueeRow2', this.list2);
+				}, 100);
+			});
 		},
 		onLoad(option) {
-			window.onload = () => {
-				this.$nextTick(() => {
-					this.adjustMarqueeSpeed('row1', this.list1);
-					this.adjustMarqueeSpeed('row2', this.list2);
-				});
-			};
-			let that = this;
-			this.isShowBottom = option.showBottomOpenVip == "1"
+			this.isShowBottom = option.showBottomOpenVip === "1";
 			window.showBottomOpenVip = (str) => {
-				// alert("str:" + str)
 				console.log("iOS 主动推送的字符串：", str);
-				that.isShowBottom = str == "1"
-
+				this.isShowBottom = str === "1";
 			};
-			let params = {
 
-			}
-
-			this.$u.api.getBullect(params)
-				.then(res => {
-					// alert("res:" + res)
-					// that.videoList = [];
-					if (res.code == 200) {
-						var list = res.body;
-						var length = list.length;
-						that.list1 = list.splice(0, length / 2);
-						that.list2 = list.splice(-length / 2);
-
-					}
-
-				})
+			// 模拟异步获取数据（根据你的实际接口调整）
+			let params = {}
+			this.$u.api.getBullect(params).then(res => {
+				if (res.code == 200) {
+					const list = res.body;
+					const len = list.length;
+					this.list1 = list.slice(0, len / 2);
+					this.list2 = list.slice(-len / 2);
+					this.$nextTick(() => {
+						setTimeout(() => {
+							this.startMarquee('marqueeRow1', this.list1);
+							this.startMarquee('marqueeRow2', this.list2);
+						}, 100);
+					});
+				}
+			});
 		},
 		onReady() {
 			uni.setNavigationBarTitle({
 				title: '驾考指南'
 			});
 		},
+		beforeDestroy() {
+			// 清除动画，防止内存泄漏
+			this.stopMarquee(1);
+			this.stopMarquee(2);
+		},
 		methods: {
-			getObject() {
+			waitForImagesLoaded(container) {
+				const imgs = container.querySelectorAll('image, img');
+				const promises = Array.from(imgs).map(img => {
+					return new Promise(resolve => {
+						if (img.complete) {
+							resolve();
+						} else {
+							img.onload = img.onerror = () => resolve();
+						}
+					});
+				});
+				return Promise.all(promises);
+			},
 
-			},
-			goBack() {
-				uni.navigateBack();
-			},
-			adjustMarqueeSpeed(refName, list) {
+			startMarquee(refName, list) {
 				const row = this.$refs[refName];
 				if (!row) return;
 
-				// ✅ 关键：使用 row.$el 获取原生 DOM 元素
-				const rowElement = row.$el || row; // 兼容某些平台直接是 DOM 的情况
+				const content = row.$el || (row.length ? row[0] : row);
+				const innerContent = content.querySelector('.marquee-content') || content;
 
-				// 现在可以正常使用 querySelectorAll
-				const items = rowElement.querySelectorAll('.marquee-item');
-				if (!items.length) return;
+				this.waitForImagesLoaded(innerContent).then(() => {
+					const items = Array.from(innerContent.querySelectorAll('.marquee-item')).slice(0, list.length);
+					if (items.length === 0) return;
 
-				// 计算第一份内容的总宽度
-				const totalWidth = Array.from(items)
-					.slice(0, list.length)
-					.reduce((sum, el) => sum + el.offsetWidth, 0);
+					const totalWidth = items.reduce((sum, el) => {
+						const style = getComputedStyle(el);
+						return sum + el.offsetWidth + parseFloat(style.marginRight || 0);
+					}, 0);
 
-				// 获取 margin-right
-				const firstItem = items[0];
-				const style = getComputedStyle(firstItem);
-				const marginRight = parseFloat(style.marginRight) || 0;
-				const spacingWidth = marginRight * (list.length - 1);
+					// 清除之前的动画
+					this.stopMarquee(refName.slice(-1));
 
-				const contentWidth = totalWidth + spacingWidth;
+					let position = 0;
+					const animate = () => {
+						position -= this.marqueeSpeed;
+						if (position <= -totalWidth) position = 0;
+						innerContent.style.transform = `translateX(${position}px)`;
+						this[`marqueeAnimId${refName.slice(-1)}`] = requestAnimationFrame(animate);
+					};
 
-				// 滚动速度（像素/秒）
-				const scrollSpeed = 25000;
-				const duration = contentWidth / scrollSpeed;
-				console.log("duration:" + duration + ",contentWidth:" + contentWidth)
-
-				// 设置 CSS 变量
-				rowElement.style.setProperty('--duration', `${Math.max(8, duration)}s`);
+					animate();
+				});
 			},
+
+			stopMarquee(id) {
+				const key = `marqueeAnimId${id}`;
+				if (this[key]) {
+					cancelAnimationFrame(this[key]);
+					this[key] = null;
+				}
+			},
+
+			// 其他方法保持不变
 			toOpenVipPage() {
-				callNative('toOpenVipPage')
+				callNative('toOpenVipPage');
 			},
 			toLivePage() {
-				callNative('toLivePage')
+				callNative('toLivePage');
 			},
 			toSkillPage() {
-				callNative('toSkillPage')
+				callNative('toSkillPage');
 			},
 			toMkExamPage() {
-				callNative('toTrueMockExamPage')
+				callNative('toTrueMockExamPage');
 			},
+			goBack() {
+				uni.navigateBack();
+			}
 		}
-	}
+	};
 </script>
 
 <style lang="scss">
+	// ===== 基础样式 =====
 	.container {
 		background-color: #f8f9fa;
-		/* padding: 0 20rpx; */
 		padding-bottom: 90rpx;
 	}
 
 	.container2 {
 		background-color: #f8f9fa;
-		/* padding: 0 20rpx; */
 		padding-bottom: 30rpx;
-	}
-
-	.header {
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		height: 80rpx;
-		background: #ffffff;
-		border-bottom: 1rpx solid #e5e5e5;
-		padding: 0 20rpx;
-	}
-
-	.back {
-		font-size: 40rpx;
-		color: #333;
-	}
-
-	.title {
-		font-weight: bold;
-		font-size: 36rpx;
-		color: #333;
 	}
 
 	.welcome {
@@ -314,11 +278,7 @@
 		border-radius: 12rpx;
 		margin: 12rpx 16rpx;
 
-		/* background: linear-gradient(135deg, #e6f7ff, #d6f2ff); */
-
 		.welcome_bg {
-			/* 	position: absolute;
-			z-index: -1; */
 			width: 100%;
 			height: 128rpx;
 		}
@@ -326,280 +286,45 @@
 		text {
 			position: absolute;
 			z-index: 1;
-			left: 0rpx;
-			top: 0rpx;
+			left: 0;
+			top: 0;
 			text-indent: 2em;
 			margin: 24rpx;
-			/* padding-left: 20rpx;
-			padding-right: 20rpx; */
-			/* margin: 20rpx 20rpx; */
-			font-size: 14rpx;
-			text-align: start;
-			color: #344BA7;
 			font-size: 28rpx;
+			color: #344BA7;
 			line-height: 1.5;
 		}
-
 	}
 
 	.swiper_wrap {
 		width: 100%;
 		height: 280rpx;
 
-		/* 根据内容设置Swiper的高度 */
 		.swiper-item-image {
 			width: 100%;
 			height: 100%;
 		}
 	}
 
-
-
-
 	.quick-learning {
-
-		/* background: #007aff; */
 		border-radius: 12rpx;
 		padding: 20rpx;
-		color: white;
 	}
 
-	.quick-title {
-		font-size: 36rpx;
-		font-weight: bold;
-		text-align: center;
-		margin-bottom: 10rpx;
-	}
-
-	.steps {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		gap: 10rpx;
-	}
-
-	.step-item {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		padding: 10rpx;
-		border-radius: 8rpx;
-		background: rgba(255, 255, 255, 0.2);
-	}
-
-	.step-item.active {
-		background: rgba(255, 255, 255, 0.4);
-	}
-
-	.step-num {
-		width: 40rpx;
-		height: 40rpx;
-		border-radius: 50%;
-		background: white;
-		color: #007aff;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		font-weight: bold;
-	}
-
-	.step-text {
-		margin-top: 5rpx;
-		font-size: 24rpx;
-		color: white;
-	}
-
-	.arrow {
-		font-size: 32rpx;
-		color: white;
-	}
-
-	.score-list {
-		margin: 20rpx 0;
-		display: flex;
-		flex-wrap: wrap;
-		gap: 10rpx;
-	}
-
-	.score-item {
-		background: #fff;
-		border-radius: 8rpx;
-		padding: 10rpx 20rpx;
-		box-shadow: 0 2rpx 6rpx rgba(0, 0, 0, 0.1);
-		font-size: 24rpx;
-		color: #555;
-	}
-
-	.video-container {
-		position: relative;
-		width: calc(100% - 48rpx);
-		/* 计算宽度，减去左右边距的总和 */
-		height: 300rpx;
-		border-radius: 12rpx;
-		overflow: hidden;
-		margin: 0rpx 24rpx;
-		margin-top: 20rpx;
-		// background: #eee;
-	}
-
-
-	.video-bg {
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
-	}
-
-	.video-overlay {
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		background: rgba(0, 0, 0, 0.3);
-		color: white;
-		font-size: 60rpx;
-	}
-
-	.video-progress {
-		position: absolute;
-		bottom: 10rpx;
-		left: 0;
-		right: 0;
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		padding: 0 20rpx;
-		color: white;
-		font-size: 24rpx;
-	}
-
-	.progress-bar {
-		flex: 1;
-		height: 6rpx;
-		background: rgba(255, 255, 255, 0.3);
-		border-radius: 3rpx;
-		margin: 0 10rpx;
-		overflow: hidden;
-	}
-
-	.progress {
-		width: 30%;
-		height: 100%;
-		background: #007aff;
-	}
-
-	.card-list {
-
-		display: flex;
-		flex-direction: column;
-		margin: 20px 24rpx;
-		gap: 20rpx;
-		/* 推荐使用 gap 代替 margin-bottom */
-	}
-
-	.card-item {
-		position: relative;
-		width: 100%;
-		// height: 110rpx;
-		background: white;
-		border-radius: 12rpx;
-		padding: 0rpx;
-		// margin-bottom: 20rpx;
-		// margin-left: 24rpx;
-		// margin-right: 24rpx;
-		margin-top: 10rpx;
-		overflow: hidden;
-		/* 确保图片圆角显示正常 */
-		// box-shadow: 0 2rpx 6rpx rgba(0, 0, 0, 0.1);
-	}
-
-	.card-item-image {
-		width: 100%;
-		height: auto;
-		display: block;
-		/* 消除图片底部间隙 */
-	}
-
-	.card-header {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		margin-bottom: 10rpx;
-	}
-
-	.card-title {
-		font-size: 32rpx;
-		font-weight: bold;
-		color: #333;
-	}
-
-	.tag {
-		background: #007aff;
-		color: white;
-		padding: 4rpx 12rpx;
-		border-radius: 12rpx;
-		font-size: 20rpx;
-	}
-
-	.card-body {
-		margin-bottom: 10rpx;
-	}
-
-	.desc {
-		color: #666;
-		font-size: 24rpx;
-	}
-
-	.card-footer {
-		text-align: right;
-	}
-
-	.more {
-		color: #007aff;
-		font-size: 24rpx;
-	}
-
-	.footer-banner {
-		position: fixed;
-		width: 100%;
-		height: 100rpx;
-		bottom: 0;
-		left: 0;
-		background: #e63737;
-		color: white;
-		padding: 20rpx;
-		text-align: center;
-		// border-radius: 12rpx;
-
-		font-size: 32rpx;
-	}
-
+	// ===== 跑马灯样式 =====
 	.double-marquee-container {
-		// height: 55rpx;
 		width: 100%;
 		overflow: hidden;
-		background-color: #fff;
 		border-radius: 12px;
-		// box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
 		display: flex;
 		flex-direction: column;
-		gap: 15rpx; // 两行之间的间距，防止重叠
+		gap: 15rpx;
 	}
 
 	.marquee-row {
-		// height: 44px;
 		display: flex;
 		align-items: center;
-		// padding: 0 12rpx;
 		background-color: #f8f9fa;
-		// border-bottom: 1px solid #e9ecef;
-
-		&:last-child {
-			border-bottom: none;
-		}
 
 		&:first-child {
 			border-radius: 12rpx 12rpx 0 0;
@@ -613,21 +338,9 @@
 	.marquee-content {
 		display: flex;
 		white-space: nowrap;
-		animation: marquee 15s linear infinite;
-		animation-delay: 0s;
 		will-change: transform;
 		justify-content: flex-start;
-	}
-
-	// ✅ 真正无缝的关键：移动 -50%
-	@keyframes marquee {
-		0% {
-			transform: translateX(0);
-		}
-
-		100% {
-			transform: translateX(-50%);
-		}
+		// ❌ 移除 CSS 动画，使用 JS 控制
 	}
 
 	.marquee-item {
@@ -635,8 +348,7 @@
 		display: flex;
 		align-items: center;
 		margin-right: 60rpx;
-		padding: 0rpx 14rpx;
-		// background-color: #e3f2fd;
+		padding: 0 14rpx;
 		background: linear-gradient(to right, #FFEEC8, #FFFCF7);
 		border-radius: 22px;
 		font-size: 26rpx;
@@ -646,10 +358,7 @@
 			width: 32rpx;
 			height: 32rpx;
 			margin-right: 6px;
-			font-size: 16px;
 		}
-
-		.msg {}
 
 		.msg-text-color-1 {
 			color: #000000CC;
@@ -659,5 +368,55 @@
 			color: #000000;
 			font-weight: 500;
 		}
+	}
+
+	// ===== 其他样式 =====
+	.video-container {
+		width: calc(100% - 48rpx);
+		height: 300rpx;
+		border-radius: 12rpx;
+		margin: 0 24rpx;
+		margin-top: 20rpx;
+	}
+
+	.video-bg {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+
+	.card-list {
+		display: flex;
+		flex-direction: column;
+		margin: 20px 24rpx;
+		gap: 20rpx;
+	}
+
+	.card-item {
+		position: relative;
+		width: 100%;
+		background: white;
+		border-radius: 12rpx;
+		overflow: hidden;
+		margin-top: 10rpx;
+	}
+
+	.card-item-image {
+		width: 100%;
+		height: auto;
+		display: block;
+	}
+
+	.footer-banner {
+		position: fixed;
+		width: 100%;
+		height: 100rpx;
+		bottom: 0;
+		left: 0;
+		background: #e63737;
+		color: white;
+		padding: 20rpx;
+		text-align: center;
+		font-size: 32rpx;
 	}
 </style>
