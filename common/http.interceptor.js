5e7d18b249f894efd55ba810f4d4d46cc40f0255
hym <uffanv@gmail.com>
2025-10-16 08:47:04 +0800

    接口隐藏弹框

--- common/http.interceptor.js
+++ common/http.interceptor.js
@@ -34,7 +34,7 @@ const install = (Vue, vm) => {
 		// baseUrl: 'http://192.168.2.204:8080',
 		loadingText: '加载中~~~',
 		// dataType: 'json',
-		showLoading: true, // 是否显示请求中的loading
+		showLoading: false, // 是否显示请求中的loading
 		loadingTime: 800, // 在此时间内，请求还没回来的话，就显示加载中动画，单位ms
 		originalData: false, // 是否在拦截器中返回服务端的原始数据
 		loadingMask: true,
